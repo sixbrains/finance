68a3cb170c0750e7ca5a46ac9f4f7f27e246883c
sixbrains <sixbrains@gmail.com>
2025-08-11 15:55:32 -0500

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,8 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = {
-  appId: 'com.tuempresa.gastos',
-  appName: 'Gastos App',
-  webDir: 'dist',
-  bundledWebRuntime: false
-};
-export default config;
